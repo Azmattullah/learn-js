e6f0cb091a39b6249e45b068f0964498354c746b
Azmattullah <137893775+Azmattullah@users.noreply.github.com>
2023-08-22 06:27:31 +0000

    dataType

---
+++ basic_01/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 909090
+let accoutEmail = "azmattullah@gmail.com"
+var accountPassword = "123456"
+accountCity = "jamshedpur"
+let accountState
+accountState = "Azmattullah"
+
+console.log(accountState)
+
+// console.table([accountId, accoutEmail, accountPassword, accountCity])
+
+// accountId = 890
+accoutEmail = "tullahazmat@gmail.com"
+accountPassword = "9087654321"
+accountCity = "kolkata"
+// console.table([accountId, accoutEmail, accountPassword, accountCity])
+
+// console.log(accoutEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+
+/*
+This is comment.
+prefere not to use var data type
+because of issue in block scope and functional scope
+Insted of var use let and const
+*/
---
+++ basic_01/02_dataType.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+// alert("Azmattullah") 
+we cant use alert in node js 
+it work on browser 
+*/
+
+// // code readablity should be high priroty
+// console.log(6 + 9);
+// console.log("Azmattullah");
+
+let name = "Azmattullah"
+let age = 23
+let isLoggedIn = false
+
+// console.table([name, age, isLoggedIn])
+
+/*
+number => range is 2 power 53
+bigint
+string => "Kolkata is city of Joy"
+boolen => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
+// object
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isLoggedIn)
+console.log(typeof undefined)
+console.log(typeof null)
